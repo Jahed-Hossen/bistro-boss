f1f5298abaa466cc8aa3b715847a1077df077d05
jahedhossen1 <jahedshomadan@gmail.com>
2023-12-16 23:36:37 +0600

    order page ready

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -1,14 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../shared/Cover/Cover';
 import order from '../../../assets/shop/banner2.jpg'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import useMenu from '../../shared/Hookes/useMenu';
+import Foodcart from '../../shared/FOOD CART/Foodcart';
 const Order = () => {
+    const [tabIndex, setTabIndex]=useState(0)
+    const [menu]=useMenu()
+
+    const dessert = menu.filter(item => item.category === 'dessert')
+    const pizza = menu.filter(item => item.category === 'pizza')
+    const salad = menu.filter(item => item.category === 'salad')
+    const soup = menu.filter(item => item.category === 'soup')
+    const drinks = menu.filter(item => item.category === 'drinks')
     return (
         <div>
-         <Helmet>
-        <title>Bistro Boss | Order Food</title>
-      </Helmet>
-      <Cover img={order} tittle={'ORDER FOOD'} details={'Would you like to try a dish?'}></Cover>
+           <div className=' pb-8'>
+           <Helmet>
+                <title>Bistro Boss | Order Food</title>
+            </Helmet>
+            <Cover img={order} tittle={'ORDER FOOD'} details={'Would you like to try a dish?'}></Cover>
+           </div>
+             <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+                    <TabList>
+                        <Tab>SALAD</Tab>
+                        <Tab>PIZZA</Tab>
+                        <Tab>SOUPS</Tab>
+                        <Tab>DESSEERTS</Tab>
+                        <Tab>DRINKS</Tab>
+                    </TabList>
+                    <TabPanel>
+                       <div className='grid sm:grid-cols-1 md:grid-col-2 lg:grid-cols-3 py-8 gap-10'>
+                       {salad.map(item=><Foodcart
+                        key={item._id}
+                        item={item}
+                        ></Foodcart>)}
+                       </div>
+                        </TabPanel>
+                    <TabPanel>
+                    <div className='grid sm:grid-cols-1 md:grid-col-2 lg:grid-cols-3 py-8 gap-10'>
+                       {pizza.map(item=><Foodcart
+                        key={item._id}
+                        item={item}
+                        ></Foodcart>)}
+                       </div>
+                    </TabPanel>
+                    <TabPanel>
+                    <div className='grid sm:grid-cols-1 md:grid-col-2 lg:grid-cols-3 py-8 gap-10'>
+                       {soup.map(item=><Foodcart
+                        key={item._id}
+                        item={item}
+                        ></Foodcart>)}
+                       </div>
+                    </TabPanel>
+                    <TabPanel>
+                    <div className='grid sm:grid-cols-1 md:grid-col-2 lg:grid-cols-3 py-8 gap-10'>
+                       {dessert.map(item=><Foodcart
+                        key={item._id}
+                        item={item}
+                        ></Foodcart>)}
+                       </div>
+                    </TabPanel>
+                    <TabPanel>
+                    <div className='grid sm:grid-cols-1 md:grid-col-2 lg:grid-cols-3 py-8 gap-10'>
+                       {drinks.map(item=><Foodcart
+                        key={item._id}
+                        item={item}
+                        ></Foodcart>)}
+                       </div>
+                    </TabPanel>
+                   
+               </Tabs>
+               
         </div>
     );
 };
---
+++ src/Pages/shared/FOOD CART/Foodcart.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Foodcart = ({item}) => {
+
+    const {image,price,recipe,name}= item;
+    return (
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+            <figure><img src={image} alt="Shoes" /></figure>
+            <p className=' absolute bg-black right-0 text-white rounded-sm px-1 mr-5 mt-5 '>{price}</p>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <p>{recipe}</p>
+                
+                <div className="card-actions justify-end">
+                    <button className="btn btn-outline border-0 border-b-4">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Foodcart;
